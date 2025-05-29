e0e877ee9737a9cc8c6c29a040f787d70c91fc68
joaomdp <joaomdp.dev@gmail.com>
2025-05-28 21:43:37 -0300

    Adiciona persistência de sessão no contexto de autenticação e refatora o uso do onAuthStateChanged

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,10 @@
 import { createContext, useEffect, useState } from "react";
-import { onAuthStateChanged, signOut } from "firebase/auth";
+import {
+  onAuthStateChanged,
+  signOut,
+  setPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
 import { auth } from "../firebaseConfig";
 
 const AuthContext = createContext();
@@ -7,6 +12,23 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [usuario, setUsuario] = useState(null);
   const [carregando, setCarregando] = useState(true);
+
+  useEffect(() => {
+    const setupAuth = async () => {
+      await setPersistence(auth, browserSessionPersistence);
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        setUsuario(user);
+        setCarregando(false);
+      });
+      return unsubscribe;
+    };
+
+    const unsubscribePromise = setupAuth();
+
+    return () => {
+      unsubscribePromise.then((unsubscribe) => unsubscribe());
+    };
+  }, []);
 
   const logout = () => {
     signOut(auth)
@@ -17,15 +39,6 @@ export const AuthProvider = ({ children }) => {
         console.error("Erro ao fazer logout: ", error);
       });
   };
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUsuario(user);
-      setCarregando(false);
-    });
-
-    return () => unsubscribe();
-  }, []);
 
   return (
     <AuthContext.Provider value={{ usuario, carregando, logout }}>
